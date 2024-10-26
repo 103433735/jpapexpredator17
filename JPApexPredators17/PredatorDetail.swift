56cac26651fd905e76611ba44f3ae3ef9ff11ee2
DucNguyen <ducnafs1579@gmail.com>
2024-10-27 00:15:29 +0700

    Lesson 9

---
+++ JPApexPredators17/PredatorDetail.swift
@@ -0,0 +1,18 @@
+//
+//  PredatorDetail.swift
+//  JPApexPredators17
+//
+//  Created by Hartzed Story on 10/26/24.
+//
+
+import SwiftUI
+
+struct PredatorDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PredatorDetail()
+}
